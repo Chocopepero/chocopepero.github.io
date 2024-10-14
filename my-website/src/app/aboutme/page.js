c18e489ca8861a52b77cd863944172f38239b171
Samuel Kim <36939842+Chocopepero@users.noreply.github.com>
2024-10-14 20:08:34 +0000

    Switched to React

---
+++ my-website/src/app/aboutme/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+export default function AboutMePage() {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen"> 
+      <img 
+      src= "/images/lebum.png"
+      width = {500}
+      height = {500}
+      ></img>
+      <h1 className='text-9xl'>ME AM FUN-GI HAHAHA</h1>
+    </div>
+   
+    );
+  }

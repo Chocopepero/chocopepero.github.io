76f4975b5b01a86c1cea913977c0a3f9addedab1
Samuel Kim <36939842+Chocopepero@users.noreply.github.com>
2024-11-12 03:58:14 +0000

    Cursor still doesnt work, github icon needs to be centered

--- my-website/src/app/page.js
+++ my-website/src/app/page.js
@@ -20,13 +20,31 @@ export default function Home() {
     <div className="flex flex-col min-h-screen cursor">
       <div className="flex items-center justify-center flex-grow">
         <main className="flex flex-col gap-8 items-center justify-center sm:items-start text-center">
-          <Image
-            src="/images/WorryVeryDepressed.png"
-            width={360}
-            height={100}
-            priority
-            className="mx-auto"
-          />
+          <div className="author">
+            <Image
+              src="/images/WorryVeryDepressed.png"
+              width={132}
+              height={132}
+              priority
+              className="author"
+            />
+            <div className="author-name">
+              <a
+                href="/">
+                Test Name
+              </a>
+              <a href="https://github.com/Chocopepero" target="_blank" className="author-links">
+                <img
+                  className="author-links"
+                  src="/images/github-mark-white.png"
+                  width={32}
+                  height={32}
+                >
+                </img>
+              </a>
+            </div>
+
+          </div>
           <h1 className="flex items-center justify-between p-4 bg-blue-500">HEY KIDS YOU WANA SEE A DEAD BODY?</h1>
           <div className="flex gap-4 items-center flex-col sm:flex-row">
             <a
@@ -58,6 +76,7 @@ export default function Home() {
       <footer className="footer">
         <a
           onClick={handleAboutMeRedirect}
+          className="footer-link"
         >
           <Image
             aria-hidden
@@ -70,6 +89,7 @@ export default function Home() {
         </a>
         <a
           onClick={handleProjectRedirect}
+          className="footer-link"
         >
           <Image
             aria-hidden
@@ -84,6 +104,7 @@ export default function Home() {
           href="mailto:samkunkim@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
+          className="footer-link"
         >
           <Image
             aria-hidden

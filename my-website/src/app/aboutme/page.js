7c8c79f41dd6df80815b504a8c973426aca0a3ee
Samuel Kim <36939842+Chocopepero@users.noreply.github.com>
2024-12-09 02:09:08 +0000

    Added Transitions and started formatting pages

--- my-website/src/app/aboutme/page.js
+++ my-website/src/app/aboutme/page.js
@@ -1,15 +1,37 @@
-'use client';
+"use client";
 
 export default function AboutMePage() {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen"> 
-      <img 
-      src= "/images/lebum.png"
-      width = {500}
-      height = {500}
-      ></img>
-      <h1 className='text-9xl'>ME AM FUN-GI HAHAHA</h1>
+  return (
+    <div className="container">
+      <div className="bio">
+        <img src="/images/lebum.png" className="responsive-image"></img>
+        <br></br>
+
+        <h1>
+          I’m Samuel Kim, a passionate and driven individual transitioning from
+          a career as a Pharmacy Technician at San Antonio Regional Hospital to
+          pursuing my love for technology as a Computer Science student at
+          California State University, Fullerton.
+          <br></br><br></br>
+          My experience in the
+          healthcare industry has equipped me with a unique blend of skills.
+          I’ve developed the ability to work within a team under
+          high-pressure environments, ensuring precision and efficiency in every
+          task. At the same time, I’ve honed my critical thinking and
+          problem-solving abilities, empowering me to make independent decisions
+          and adapt to new challenges. 
+          <br></br><br></br>
+          Now, I’m utilizing those skills in the
+          world of computer science, where I’m eager to combine my analytical
+          mindset with technical expertise. I’m particularly passionate about
+          leveraging technology to create innovative solutions that improve
+          lives, whether it’s through developing software, designing systems, or
+          tackling complex problems.
+        </h1>
+        <h1>
+        <a className="bio-link" href="/documents/Samuel_Kim_Resume.pdf" download>Download my resume by clicking here!</a>
+        </h1>
+      </div>
     </div>
-   
-    );
-  }
+  );
+}
---
+++ my-website/src/components/Navbar.js
@@ -0,0 +1,49 @@
+// src/components/Navbar.js
+"use client";
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+const Navbar = () => {
+  const currentPath = usePathname();
+
+  return (
+    <div className="topnav" id="myTopnav">
+      <Link href="/" className={currentPath === '/' ? 'active' : ''}>
+        Home
+      </Link>
+      <Link href="/aboutme" className={currentPath === '/aboutme' ? 'active' : ''}>
+        <Image
+          aria-hidden
+          src="https://nextjs.org/icons/file.svg"
+          alt="File icon"
+          width={16}
+          height={16}
+        />
+        About me
+      </Link>
+      <Link href="/projects" className={currentPath === '/projects' ? 'active' : ''}>
+        <Image
+          aria-hidden
+          src="https://nextjs.org/icons/window.svg"
+          alt="Window icon"
+          width={16}
+          height={16}
+        />
+        My Projects
+      </Link>
+      <Link href="/contact" className={currentPath === '/contact' ? 'active' : ''}>
+        <Image
+          aria-hidden
+          src="https://nextjs.org/icons/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
+        />
+        Contact Me
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
